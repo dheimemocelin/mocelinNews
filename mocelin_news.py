342479f3341cb3549ffd36a69177b86e36e2eeb3
dheimemocelin <dheime7@gmail.com>
2023-07-04 14:27:38 -0400

    atualizaçao

--- mocelin_news.py
+++ mocelin_news.py
@@ -17,7 +17,7 @@ class MocelinNews:
         self.dict_sites = {}
         self.all_sites = [ 'veja', 'r7', 'globo']
         
-        self.screan = 0
+        self.screen = 0
         self.kill = False
         
         self.page = 1
@@ -52,108 +52,137 @@ class MocelinNews:
         while command.lower() not in valid_commands and not timed:
             print('Comando inválido. Digite novamente\n')
             command, timed = timedInput('>>', timeout)
-        command = 0 if command == '' else command
+        command = '0' if command == '' else command
+        return command
     
     def main_loop(self):
         while True:
             os.system('cls' if os.name == 'nt' else 'clear')
-        
-            match self.screan:
+
+            match self.screen:
                 case 0:
-                    print('SEJA BEM VINDO AO MOCELIN NEWS')
+                    print('SEJA BEM VINDO AO MOCELIN NEWS.')
                     print('Por favor escolha algum item do menu')
                     print('')
-                    print("1. Ultimas noticias\n2. Adicionar site\n3. Remover site\n4. Fechar programa")
-                    
-                    self.screan = int(self._receive_command([ '1', '2', '3'], 5))
-                    
+                    print("1. Ultimas Noticias\n2. Adicionar site\n3. Remover sites\n4. Fechar o Programa")
+
+                    self.screen = int(self._receive_command(['1', '2', '3', '4'], 5))
+                    print(self.screen , type(self.screen ))
+
                 case 1:
                     self.display_news()
-                    command = self._receive_command(['p', 'a', 'l', 'v'], 500)
+                    command = self._receive_command(['p', 'a', 'l', 'v'], 5)
+                    match command:
+                        case 'p':
+                            if self.page < self.max_page: self.page += 1
+                        
+                        case 'a':
+                                if self.page > 1: self.page -= 1
+                        
+                        case 'v':
+                            self.screen = 0
+                            continue
+                    
+                        case 'l':
+                            link = int(input('>> Insira o numero da materia que deseja abrir: '))
+                            if link < 1 or link > len(self.filtered_news):
+                                print('Materia inexistente!')
+                            else:
+                                webbrowser.open(self.filtered_news[link-1]['link'])
+                                # if platform == "linux" or platform == "linux2": # Linux
+                                #     chrome_path = '/usr/bin/google-chrome %s'
+                                # elif platform == "darwin": # OS X
+                                #     chrome_path = 'open -a /Applications/Google\ Chrome.app %s'
+                                # elif platform == "win32": # Windows
+                                #     chrome_path = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+                                # webbrowser.get(f"\"{chrome_path}\" %s").open(link)
+
                 case 2:
                     os.system('cls' if os.name == 'nt' else 'clear')
-                    print('Digite o numero do site que deseja adicinar para lista de sites ativios.\nPrecione 0 para voltar para o menu')
-                    print('\tSITES ATIVOS==========\n')
+                    print('Digite o numero do site que deseja adicionar para a lista de sites ativos.\nPressione 0 para voltar para o menu.')
+                    print("\tSITES ATIVOS ===============\n")
                     for i in self.sites:
                         print('\t', i)
 
-                    print('\tSITES INATIVOS==========\n')
-                    offiline_sites = [i for i in self.all_sites if i not in self.sites]
-                    for i in range(len(offiline_sites)):
-                        print(f'\t{i+1}. {offiline_sites[i]}')
-                    site = int(self._receive_command([str(i) for i in range(len(offiline_sites)+1)], 50))
-                    
+                    print("\n\tSITES INATIVOS =============")
+                    offline_sites = [i for i in self.all_sites if i not in self.sites]
+                    for i in range(len(offline_sites)):
+                        print(f'\t{i+1}. {offline_sites[i]}')
+                    site= int(self._receive_command([str(i) for i in range(len(offline_sites)+1)], 50))
+
                     if site == 0:
-                        self.screan=0
+                        self.screen=0
                         continue
-                    self.sites += [offiline_sites[site-1]]
+                    self.sites += [offline_sites[site-1]]
                     self._update_file(self.sites, 'sites')
-                    
-                    
+
                 case 3:
                     os.system('cls' if os.name == 'nt' else 'clear')
-                    print('Digite o numero do site para remove-lo. Caso queira voltar para o menu, digite0\n')
+                    print('Digite o numero do site para remove-lo. Caso queira voltar para o Menu, digite 0\n')
                     for i in range(len(self.sites)):
                         print(f'\t{i+1}. {self.sites[i]}')
-                    site = int(self._receive_command([str(i) for i in range(len(self.sites)+1)], 50))
-                    
+                    site= int(self._receive_command([str(i) for i in range(len(self.sites)+1)], 50))
                     if site == 0:
-                        self.screan=0
+                        self.screen=0
                         continue
-                    
+
                     del self.sites[site-1]
                     self._update_file(self.sites, 'sites')
-                    
-                    
+
                 case 4:
                     self.kill = True
                     sys.exit()
-        
+            
     def display_news(self):
         os.system('cls' if os.name == 'nt' else 'clear')
         print(f"Último update: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-        
-        self.filtered_news = [ i for i in self.news if i["fonte"] in self.sites]
+
+        self.filtered_news = [i for i in self.news if i["fonte"] in self.sites]
         self.max_page = ceil(len(self.filtered_news) / 20)
-        
-        if self.page > self.max_page: self.page = 1
-        
+
+        if self.page > self.max_page:
+            self.page = self.max_page  # Atualiza a página atual para o novo número máximo de páginas
+
         constante = (self.page - 1) * 10
-        
+
         for i, article in enumerate(self.filtered_news[constante:constante+10]):
-            print(f'{constante+i}. {article['data'].strftime('%Y-%m-%d %H:%M:%S')} - {article['fonte'].upper()} - {article['materia']}')
-        print(f'Page {self.page}/{self.max_page}')
-        
-        print('===========================================================================')
-        print('Comandos')
-        print('P - Poxima Pagína | A - Pagna Anterior | L - Abrir material no navegador | V - Voltar  ')
-        
+            print(f"{constante+i+1}. {article['data'].strftime('%d/%m/%Y %H:%M')} - {article['fonte'].upper()} - {article['materia']}")
+        print(f'Página {self.page}/{self.max_page}')
+
+        print('================================================================\n')
+        print('Comandos:')
+        print('P - Próxima Página | A - Página Anterior | L - Abrir matéria no navegador | V - Voltar')
+
+
+
     def update_news(self):
         while not self.kill:
             for site in self.all_sites:
-                self.dict_sites[site].update_news()
+                self.dict_site[site].update_news()
 
-                for key, value in self.dict_sites[site].news.items():
+                for key, value in self.dict_site[site].news.items():
                     dict_aux = {}
                     dict_aux['data'] = datetime.now()
-                    dict_aux['fonte'] = site
+                    dict_aux['fonte'] = site  
                     dict_aux['materia'] = key
                     dict_aux['link'] = value
-                    
+
                     if len(self.news) == 0:
                         self.news.insert(0, dict_aux)
                         continue
                     
                     add_news = True
                     for news in self.news:
-                        if dict_aux['materia'] == news['materia'] and dict_aux['fonte'] == news['fonte']:
+                        if dict_aux["materia"] == news["materia"] and dict_aux["fonte"] == news["fonte"]:
                             add_news = False
                             break
-                    
+
                     if add_news:
                         self.news.insert(0, dict_aux)
             self.news = sorted(self.news, key=lambda d: d['data'], reverse=True)
             self._update_file(self.news, 'news')
             time.sleep(10)
-            
+                
+
 self = MocelinNews()
+self.main_loop()
